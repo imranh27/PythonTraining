47633c146077ed434bec66fe9e4cf4f696236f49
imranh27 <imranhashmi27@gmail.com>
2017-07-27 20:24:01 +0100

    Started on Math and Binary Game

---
+++ Learn_Python_In_One_Day/Math_Binary/gametasks.py
@@ -0,0 +1,22 @@
+
+#Page 2281
+
+
+
+def printInstructions(instruction):
+    return print(instruction)
+
+def getUserScore(userName):
+    inputFile = open('userScores.txt', 'r')
+
+    for line in inputFile:
+        content = line.split(',')
+    
+
+
+def main():
+    printInstructions()
+
+
+if __name__ == '__main__':
+    main()
