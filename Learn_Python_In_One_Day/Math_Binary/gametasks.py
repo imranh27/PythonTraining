c54af1dc4f4dd57e5bf8777a3374152ca08ae0b1
imranh27 <imranhashmi27@gmail.com>
2017-07-31 21:17:36 +0100

    Further exercise work.

--- Learn_Python_In_One_Day/Math_Binary/gametasks.py
+++ Learn_Python_In_One_Day/Math_Binary/gametasks.py
@@ -1,17 +1,33 @@
 
 #Page 2281
 
-
-
 def printInstructions(instruction):
     return print(instruction)
 
+
 def getUserScore(userName):
+    try:
     inputFile = open('userScores.txt', 'r')
 
     for line in inputFile:
-        content = line.split(',')
-    
+        content = line.rstrip('\n').split(',')
+        person = content[0]
+        score = int(content[1])
+
+        if userName == person:
+            print("The score for {0:s} is {1:d}".format(person, score))
+            inputFile.close()
+            return '-1'
+
+    except IOError:
+    print('The file was not found, so we will create it')
+    inputFile = open('userScores.txt', 'w')
+    inputFile.close()
+    return '-1'
+
+    else:
+    inputFile.close()
+    return '-1'
 
 
 def main():
