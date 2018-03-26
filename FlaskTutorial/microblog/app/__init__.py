73ecf992d337feba9f0719996c3fb6c788eb31ae
imranh27 <imranhashmi27@gmail.com>
2018-03-26 20:48:13 +0100

    Chapter 7
    The edit form isn’t quite working right though…?

--- FlaskTutorial/microblog/app/__init__.py
+++ FlaskTutorial/microblog/app/__init__.py
@@ -1,6 +1,9 @@
 
 # entry point
 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -14,5 +17,21 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+
+    #email section, not implemented
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
+from app import routes, models, errors
 
