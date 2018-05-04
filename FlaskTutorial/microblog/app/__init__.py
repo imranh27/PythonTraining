57f7e2a67eb2a2b73dd3e0e4025c77d82d047276
imranh27 <imranhashmi27@gmail.com>
2018-05-04 13:29:39 +0100

    End of Chapter 10
    and a wee bit in to chapter 9..

--- FlaskTutorial/microblog/app/__init__.py
+++ FlaskTutorial/microblog/app/__init__.py
@@ -9,6 +9,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +18,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 if not app.debug:
 
@@ -31,6 +34,7 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+    mail = Mail(app)
 
 
 from app import routes, models, errors
