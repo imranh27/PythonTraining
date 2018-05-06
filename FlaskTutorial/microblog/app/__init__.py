85906a198c9c70f576e5e1c79e0e833b6ebae4e2
imranh27 <imranhashmi27@gmail.com>
2018-05-06 12:17:04 +0100

    End of Chapter 12
    Tweaked formatting of profile buttons and added timezone/locale support.

--- FlaskTutorial/microblog/app/__init__.py
+++ FlaskTutorial/microblog/app/__init__.py
@@ -11,6 +11,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +20,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
 
